a0a8237a2a4199159c6914ff319174690a66ec84
rdev888 <ryan.huie88@gmail.com>
2025-07-02 11:18:57 -0400

    add queue, enqueue, and peek methods to queue data structure

--- DataStructures/queue.go
+++ DataStructures/queue.go
@@ -1,14 +1,28 @@
 package DataStructures
 
-import "sync"
+import (
+	"sync"
+)
 
 type Queue struct {
 	items []any
 	mu    sync.Mutex
 }
 
-func (q *Queue) enquue(item any) {
+func (q *Queue) Enqueue(item any) {
 	q.mu.Lock()
 	defer q.mu.Unlock()
 	q.items = append(q.items, item)
+}
+
+func (q *Queue) Dequeue() any {
+	q.mu.Lock()
+	defer q.mu.Unlock()
+	last := q.items[len(q.items)-1]
+	q.items = q.items[:len(q.items)-1]
+	return last
+}
+
+func (q *Queue) Peek() any {
+	return q.items[len(q.items)-1]
 }
